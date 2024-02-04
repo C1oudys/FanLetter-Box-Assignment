ab6b0cdcb4395336e94e59b72fc11311b87eb66d
C1oudys <hymin310@gmail.com>
2024-02-04 20:00:08 +0900

    fanletter context 리팩토링 완료

--- src/context/FanLettersContext.jsx
+++ src/context/FanLettersContext.jsx
@@ -1,9 +1,14 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import fakeData from '../shared/fakeData'
 
 const FanLettersContext = createContext();
 
 export const FanLettersProvider = ({ children }) => {
   const [fanLetters, setFanLetters] = useState([]);
+
+  useEffect(() => {
+    setFanLetters(fakeData);
+  }, []);
 
   return (
     <FanLettersContext.Provider value={{ fanLetters, setFanLetters }}>
